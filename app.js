1e9d7cba3a700632ff3391b76927b8c81ae95b9f
Santiago Enriquez <santiago9107@hotmail.com>
2022-01-16 22:42:35 -0500

    Prove02

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const app = express();
 
-    if (url==='/'){
-        res.write('<html>');
-        res.write('<head><title> Enter Message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
+const adminRoutes =  require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const { use } = require('./routes/admin');
 
-    if (url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-        });
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode(302);
-        res.setHeader('Location', '/');
-        return res.end();
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> First Page</title></head>')
-    res.write('<body><h1> Hello from my Node.js Server</h1></body>')
-    res.write('</html>');
-    res.end();
+app.use((rer, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'Views', '404.html'))
 });
 
-server.listen(3000);
+app.listen(3000);
