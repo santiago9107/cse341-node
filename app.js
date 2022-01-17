95fedce2360e369cfa62c9c025f65f1ecedcdde5
Santiago Enriquez <santiago9107@hotmail.com>
2022-01-17 00:07:51 -0500

    Prove 02

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes =  require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views'); 
+
+const adminData =  require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { use } = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
+
 
 app.use((rer, res, next)=>{
     res.status(404).sendFile(path.join(__dirname, 'Views', '404.html'))
