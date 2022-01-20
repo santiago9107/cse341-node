590a410e7b40f19abcd5e9ec97ff6adeca9d8dff
Santiago Enriquez <santiago9107@hotmail.com>
2022-01-19 23:18:02 -0500

    Prove02

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,13 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(rootDir, 'Views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next)=>{
     products.push({title: req.body.title});
     res.redirect('/');
-})
+});
 
 exports.routes = router;
 exports.products = products;
