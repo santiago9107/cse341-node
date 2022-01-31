b7dc49b5952d1cbca6d7ea5d1aee3937a7176ad2
Santiago Enriquez <santiago9107@hotmail.com>
2022-01-30 22:04:42 -0500

    trying again

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const PATH = process.env.PORT || 5000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -49,7 +50,7 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(err => {
     console.log(err);
